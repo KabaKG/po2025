44a3eb10175313dfe704bc5b934be08a144f7003
KabaKG <boguniakuba@gmail.com>
2025-10-07 18:10:44 +0200

    lotto done

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,3 +1,10 @@
+import  java.util.Random;
 public class Lotto {
-
+    public static void main(String[] args) {
+        int[] tablica = new int[10];
+        for (int i = 0; i < 6; i++) {
+            tablica[i] = new Random().nextInt(48) + 1;
+        System.out.println(tablica[i]);
+        }
+    }
 }
